e4143bd55f5c168ac2c0971a0d4e05d469bb3cf3
Anton Petukhov <39304158+hydraIV@users.noreply.github.com>
2020-04-15 21:43:40 +0300

    initial

---
+++ src/blocks/modules/news/news.js
@@ -0,0 +1,62 @@
+import slickInit from "./slick-init.js";
+
+const API = 'https://ivvending.ru'; // API address
+
+class NewsSection {
+    constructor(container='.news-section') {
+        this.container = container;
+        this.data = [];
+        this.news = [];
+        this._fetchNews() // Get data
+            .then(() => this._render()) // then render
+            .then(() => slickInit()); // then mount slider
+    }
+    // Imitation of geting news from server
+    _fetchNews() {
+        return fetch(`${API}/news.json`)
+            .then(result => result.json())
+            .then(data => {
+                this.data = [...data];
+            })
+            .catch(error => console.log(error));
+    }
+    // Rendering whole news component/section
+    _render() {
+        const block = document.querySelector(this.container);
+        this.data.forEach(el => {
+            const newsItem = new NewsBlock(el);
+            this.news.push(newsItem);
+            block.insertAdjacentHTML('beforeend', newsItem.render());
+        });
+    }
+}
+class NewsBlock {
+    constructor(item) {
+        this.img = item.img;
+        this.category = item.category;
+        this.title = item.title;
+        this.content = item.content;
+        this.newsDate = item.newsDate;
+    }
+    // Rendering single news block
+    render() {
+        return `
+        <a href="#" class="news-block">
+            <div class="news-block__img-wrap">
+            <img src="${this.img}" alt="${this.title}">
+            </div>
+            <div class="news-block__category">
+            <svg width="14" height="16" viewBox="0 0 14 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5313 0.100304H1.49778C0.684368 0.100304 0.022576 0.747818 0.022576 1.54376V11.8006C0.022576 12.3563 0.363632 12.8481 0.891648 13.0527L8.20406 15.8913C8.3665 15.9546 8.53733 15.9864 8.71186 15.9864H11.5313C12.3447 15.9864 13.0065 15.3389 13.0065 14.5431V1.54376C13.0065 0.747818 12.3447 0.100304 11.5313 0.100304ZM1.3711 12.2038L2.38618 11.1548C2.57274 10.9619 2.88456 10.9538 3.08242 11.1369L6.33102 14.129L1.3711 12.2038ZM12.0229 14.5431C12.0229 14.8083 11.8024 15.0244 11.5313 15.0244H8.7377L3.75754 10.4371C3.16683 9.88977 2.23138 9.91518 1.67202 10.493L1.006 11.1813V1.54376C1.006 1.2784 1.22661 1.06255 1.49778 1.06255H11.5313C11.8024 1.06255 12.0229 1.2784 12.0229 1.54376V14.5431Z" fill="#717171"/>
+            </svg>
+            <div class="news-block__category-title">${this.category}</div>
+            </div>
+            <h2 class="news-block__header">${this.title}</h2>
+            <p class="news-block__content">${this.content}</p>
+            <div class="news-block__date">${this.newsDate}</div>
+        </a>
+        `
+    }
+}
+// Creating NewsSection class instance. It is rendered immediately after receiving data from the server. 
+const news =  new NewsSection('.news-section');
---
+++ src/blocks/modules/news/slick-init.js
@@ -0,0 +1,47 @@
+import $ from "jquery";
+import slick from "slick-carousel";
+
+const slickInit = () => {
+    // Mount slick-slider to 'container'
+    const mountSlider = (container) => {
+        $(container).slick({
+            cssEase: 'linear',
+            arrows: false,
+            swipeToSlide: true,
+            responsive: [
+            {
+                breakpoint: 10000,
+                settings: 'unslick'
+                },
+                {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    fade: false,
+                }
+                },
+                {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    fade: true,
+                    cssEase: 'linear',
+                }
+                },
+            ]
+            });
+        };  
+    // Mount slider if window width smaller then 1024px and watch for resize event to remount slider
+        if (window.innerWidth < 1024) {
+            mountSlider('.news-section');
+        }
+        window.addEventListener('resize', () => {
+        if(!$('.news-section').hasClass('slick-initialized')) {
+            mountSlider('.news-section');
+        }
+        });
+    }
+
+export default slickInit;
